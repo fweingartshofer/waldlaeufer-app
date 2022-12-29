7a1c814751c71869b447bc07e4c2a6b64cdb6b4a
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2022-12-29 13:20:01 +0100

    init commit

---
+++ waldlaeufer/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  waldlaeufer
+//
+//  Created by Florian Weingartshofer on 29.12.22.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ waldlaeufer/waldlaeuferApp.swift
@@ -0,0 +1,18 @@
+//
+//  waldlaeuferApp.swift
+//  waldlaeufer
+//
+//  Created by Florian Weingartshofer on 29.12.22.
+//
+//
+
+import SwiftUI
+
+@main
+struct waldlaeuferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
